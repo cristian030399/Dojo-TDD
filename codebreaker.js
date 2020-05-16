6f059ee80e6be4df596789a31492f6ca7cf7bcbf
Cristian <cristiand.vargas@udea.edu.co>
2020-05-16 11:17:45 -0500

    First commit

---
+++ codebreaker.js
@@ -0,0 +1,97 @@
+function guess(a, b, c, d) {
+    let response = ''
+
+    if (a == 4)
+        response = response + 'X'
+    if (b == 5)
+        response = response + 'X'
+    if (c == 3)
+        response = response + 'X'
+    if (d == 2)
+        response = response + 'X'
+
+    if (a != 4 && (b == 4 || c == 4 || d == 4))
+        response = response + '_'
+    if (b != 5 && (a == 5 || c == 5 || d == 5))
+        response = response + '_'
+    if (c != 3 && (a == 3 || b == 3 || d == 3))
+        response = response + '_'
+    if (d != 2 && (a == 2 || b == 2 || c == 2))
+        response = response + '_'
+    return response;
+}
+
+function guessCharacter(character) {
+    let response = ''
+    mySplitResult = character.split("");
+    let a = mySplitResult[0]
+    let b = mySplitResult[1]
+    let c = mySplitResult[2]
+    let d = mySplitResult[3]
+
+    if (a == 4)
+        response = response + 'X'
+    if (b == 5)
+        response = response + 'X'
+    if (c == 3)
+        response = response + 'X'
+    if (d == 2)
+        response = response + 'X'
+
+    if (a != 4 && (b == 4 || c == 4 || d == 4))
+        response = response + '_'
+    if (b != 5 && (a == 5 || c == 5 || d == 5))
+        response = response + '_'
+    if (c != 3 && (a == 3 || b == 3 || d == 3))
+        response = response + '_'
+    if (d != 2 && (a == 2 || b == 2 || c == 2))
+        response = response + '_'
+    return response;
+}
+
+function guessCharacterGeneric(characterToGuess, character) {
+    let response = '';
+    characterToGuessSplit = characterToGuess.split("");
+    let a = characterToGuessSplit[0]
+    let b = characterToGuessSplit[1]
+    let c = characterToGuessSplit[2]
+    let d = characterToGuessSplit[3]
+
+    characterSplit = character.split("");
+    let aa = characterSplit[0]
+    let bb = characterSplit[1]
+    let cc = characterSplit[2]
+    let dd = characterSplit[3]
+
+    if (a == aa)
+        response = response + 'X'
+    if (b == bb)
+        response = response + 'X'
+    if (c == cc)
+        response = response + 'X'
+    if (d == dd)
+        response = response + 'X'
+
+    if (a != aa && (b == aa || c == aa || d == aa))
+        response = response + '_'
+    if (b != bb && (a == bb || c == bb || d == bb))
+        response = response + '_'
+    if (c != cc && (a == cc || b == cc || d == cc))
+        response = response + '_'
+    if (d != dd && (a == dd || b == dd || c == dd))
+        response = response + '_'
+    return response;
+}
+
+function guessCharacterGenericexception(characterToGuess, character) {
+    let response = '';
+    characterToGuessSplit = characterToGuess.split("");
+
+
+}
+
+
+
+module.exports.guess = guess;
+module.exports.guessCharacter = guessCharacter;
+module.exports.guessCharacterGeneric = guessCharacterGeneric;
---
+++ codebreaker.test.js
@@ -0,0 +1,59 @@
+const code = require('./codebreaker');
+
+describe('guess operation', () => {
+    test('guess 3852 to equal X__', () => {
+        expect(code.guess(3, 8, 5, 2)).toBe('X__');
+    });
+
+    test('guess 4321 to equal X__', () => {
+        expect(code.guess(4, 3, 2, 1)).toBe('X__');
+    });
+
+    test('guess 9176 to equal ', () => {
+        expect(code.guess(9, 1, 7, 6)).toBe('');
+    });
+
+    test('guess 4576 to equal XX', () => {
+        expect(code.guess(4, 5, 7, 6)).toBe('XX');
+    });
+});
+
+describe('guessCharacter operation', () => {
+    test('guessCharacter 3852 to equal X__', () => {
+        expect(code.guessCharacter('3852')).toBe('X__');
+    });
+
+    test('guessCharacter 4321 to equal X__', () => {
+        expect(code.guessCharacter('4321')).toBe('X__');
+    });
+
+    test('guessCharacter 9176 to equal ', () => {
+        expect(code.guessCharacter('9176')).toBe('');
+    });
+
+    test('guessCharacter 4576 to equal XX', () => {
+        expect(code.guessCharacter('4576')).toBe('XX');
+    });
+});
+
+describe('guessCharacterGeneric operation', () => {
+    test('guessCharacterGeneric 3852 to equal X__', () => {
+        expect(code.guessCharacterGeneric('4532', '3852')).toBe('X__');
+    });
+
+    test('guessCharacterGeneric 4321 to equal X__', () => {
+        expect(code.guessCharacterGeneric('4532', '4321')).toBe('X__');
+    });
+
+    test('guessCharacterGeneric 9176 to equal ', () => {
+        expect(code.guessCharacterGeneric('4532', '9176')).toBe('');
+    });
+
+    test('guessCharacterGeneric 4576 to equal XX', () => {
+        expect(code.guessCharacterGeneric('4532', '4576')).toBe('XX');
+    });
+
+    test('guessCharacterGeneric 1523 to equal X__', () => {
+        expect(code.guessCharacterGeneric('1523', '0532')).toBe('X__');
+    });
+});
