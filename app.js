e2461593e86c6799407dd54f1ab45fd2090043b2
Cristian <cristiand.vargas@udea.edu.co>
2020-05-31 18:21:20 -0500

    Solución problemas de cors

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
-const app = express();
 const code = require('./codebreaker');
+const cors = require("cors");
+
+const config = {
+    application: {
+        cors: {
+            server: [{
+                origin: "http://localhost:3000", //servidor que deseas que consuma o (*) en caso que sea acceso libre
+                credentials: true
+            }]
+        }
+    }
+};
+
+const app = express();
+
+app.use(cors(
+    config.application.cors.server
+));
+
+
 
 app.get('/guessCharacterGenericException', (req, res) => {
     res.json({
